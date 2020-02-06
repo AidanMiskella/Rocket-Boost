69442a6a6440db3efadaea0d707e55fb90708dea
Aidan Miskella <aidanmiskella10@gmail.com>
2020-02-06 14:31:57 +0000

    Created level 2 and refactored sound clips

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
 
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
+    [SerializeField] AudioClip mainEngine;
 
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start() {
@@ -20,27 +22,35 @@ public class Rocket : MonoBehaviour {
     // Update is called once per frame
     void Update() {
 
-        Thrust();
-        Rotate();
+        if (state == State.Alive) {
+
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
     }
 
-    private void Thrust() {
+    private void RespondToThrustInput() {
 
         if (Input.GetKey(KeyCode.Space)) {
 
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-
-            if (!audioSource.isPlaying) {
-
-                audioSource.Play();
-            }
+            ApplyThrust();
         } else {
 
             audioSource.Stop();
         }
     }
 
-    private void Rotate() {
+    private void ApplyThrust() {
+
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+
+        if (!audioSource.isPlaying) {
+
+            audioSource.PlayOneShot(mainEngine);
+        }
+    }
+
+    private void RespondToRotateInput() {
 
         rigidBody.freezeRotation = true; // take manual control of roatation
 
@@ -59,14 +69,30 @@ public class Rocket : MonoBehaviour {
 
     private void OnCollisionEnter(Collision collision) {
 
+        if (state != State.Alive) { return; }
+
         switch (collision.gameObject.tag) {
 
             case "Friendly":
                 break;
-            case "Fuel":
+            case "Finish":
+                state = State.Transcending;
+                Invoke("LoadNextLevel", 1f);
                 break;
             default:
+                state = State.Dying;
+                Invoke("LoadFirstLevel", 1f);
                 break;
         }
+    }
+
+    private void LoadNextLevel() {
+
+        SceneManager.LoadScene(1);
+    }
+
+    private void LoadFirstLevel() {
+
+        SceneManager.LoadScene(0);
     }
 }
